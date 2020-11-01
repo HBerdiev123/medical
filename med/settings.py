2ebcdd2563b539cde905f92901f0651f1fb19e3e
khusan <berdiev.khusan@gmail.com>
2020-11-01 05:40:33 -1200

    whitenose is deployed

--- med/settings.py
+++ med/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'med.urls'
@@ -153,13 +154,15 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS =(
-#         os.path.join(BASE_DIR, 'static'),
-#     )
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS =(
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
